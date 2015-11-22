8cf612913a9ac9b89ef351e2d95154ea1ddb6fa9
lucasnodine <lucasnodine@gmail.com>
2015-11-21 18:27:43 -0600

    Lots of UI changes

---
+++ OpenLawOffice.Word/LoginDialog.Designer.cs
@@ -0,0 +1,251 @@
+﻿namespace OpenLawOffice.Word
+{
+    partial class LoginDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ErrorMessage = new System.Windows.Forms.Label();
+            this.Cancel = new System.Windows.Forms.Button();
+            this.Password = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.RememberUsername = new System.Windows.Forms.CheckBox();
+            this.Username = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.RememberServer = new System.Windows.Forms.CheckBox();
+            this.Server = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.Login = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // ErrorMessage
+            // 
+            this.ErrorMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ErrorMessage.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.ErrorMessage.Location = new System.Drawing.Point(12, 218);
+            this.ErrorMessage.Name = "ErrorMessage";
+            this.ErrorMessage.Size = new System.Drawing.Size(372, 124);
+            this.ErrorMessage.TabIndex = 35;
+            // 
+            // Cancel
+            // 
+            this.Cancel.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Cancel.Location = new System.Drawing.Point(15, 379);
+            this.Cancel.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Cancel.Name = "Cancel";
+            this.Cancel.Size = new System.Drawing.Size(368, 25);
+            this.Cancel.TabIndex = 34;
+            this.Cancel.Text = "Cancel";
+            this.Cancel.UseVisualStyleBackColor = true;
+            this.Cancel.Click += new System.EventHandler(this.Cancel_Click);
+            // 
+            // Password
+            // 
+            this.Password.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Password.Location = new System.Drawing.Point(16, 164);
+            this.Password.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Password.Name = "Password";
+            this.Password.PasswordChar = '*';
+            this.Password.Size = new System.Drawing.Size(367, 22);
+            this.Password.TabIndex = 32;
+            this.Password.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Password_KeyPress);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(13, 144);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 16);
+            this.label2.TabIndex = 31;
+            this.label2.Text = "Password";
+            // 
+            // RememberUsername
+            // 
+            this.RememberUsername.AutoSize = true;
+            this.RememberUsername.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RememberUsername.Location = new System.Drawing.Point(86, 86);
+            this.RememberUsername.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.RememberUsername.Name = "RememberUsername";
+            this.RememberUsername.Size = new System.Drawing.Size(90, 20);
+            this.RememberUsername.TabIndex = 29;
+            this.RememberUsername.Text = "Remember";
+            this.RememberUsername.UseVisualStyleBackColor = true;
+            this.RememberUsername.Visible = false;
+            // 
+            // Username
+            // 
+            this.Username.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Username.Location = new System.Drawing.Point(16, 107);
+            this.Username.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Username.Name = "Username";
+            this.Username.Size = new System.Drawing.Size(367, 22);
+            this.Username.TabIndex = 30;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 87);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 16);
+            this.label1.TabIndex = 28;
+            this.label1.Text = "Username";
+            // 
+            // RememberServer
+            // 
+            this.RememberServer.AutoSize = true;
+            this.RememberServer.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RememberServer.Location = new System.Drawing.Point(62, 31);
+            this.RememberServer.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.RememberServer.Name = "RememberServer";
+            this.RememberServer.Size = new System.Drawing.Size(90, 20);
+            this.RememberServer.TabIndex = 27;
+            this.RememberServer.Text = "Remember";
+            this.RememberServer.UseVisualStyleBackColor = true;
+            this.RememberServer.Visible = false;
+            // 
+            // Server
+            // 
+            this.Server.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Server.Location = new System.Drawing.Point(16, 52);
+            this.Server.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Server.Name = "Server";
+            this.Server.Size = new System.Drawing.Size(367, 22);
+            this.Server.TabIndex = 26;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 32);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 16);
+            this.label4.TabIndex = 25;
+            this.label4.Text = "Server";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkLabel1.Location = new System.Drawing.Point(13, 190);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(145, 16);
+            this.linkLabel1.TabIndex = 24;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Change Encryption Key";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // Login
+            // 
+            this.Login.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Login.Image = global::OpenLawOffice.Word.Properties.Resources.arrow;
+            this.Login.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.Login.Location = new System.Drawing.Point(16, 346);
+            this.Login.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Login.Name = "Login";
+            this.Login.Size = new System.Drawing.Size(367, 25);
+            this.Login.TabIndex = 33;
+            this.Login.Text = "Login";
+            this.Login.UseVisualStyleBackColor = true;
+            this.Login.Click += new System.EventHandler(this.Login_Click);
+            // 
+            // label5
+            // 
+            this.label5.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(12, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(376, 23);
+            this.label5.TabIndex = 36;
+            this.label5.Text = "OpenLawOffice: Login";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(16, 218);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(367, 25);
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Marquee;
+            this.progressBar1.TabIndex = 37;
+            this.progressBar1.Visible = false;
+            // 
+            // LoginDialog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(400, 417);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.ErrorMessage);
+            this.Controls.Add(this.Cancel);
+            this.Controls.Add(this.Login);
+            this.Controls.Add(this.Password);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.RememberUsername);
+            this.Controls.Add(this.Username);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.RememberServer);
+            this.Controls.Add(this.Server);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.linkLabel1);
+            this.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(416, 456);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(416, 456);
+            this.Name = "LoginDialog";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "OLO Login";
+            this.TopMost = true;
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.LoginDialog_KeyPress);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label ErrorMessage;
+        private System.Windows.Forms.Button Cancel;
+        private System.Windows.Forms.Button Login;
+        private System.Windows.Forms.TextBox Password;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox RememberUsername;
+        private System.Windows.Forms.TextBox Username;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox RememberServer;
+        private System.Windows.Forms.TextBox Server;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
